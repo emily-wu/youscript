5b26d47cf0862cbf9da2d7bffe4e2bd4d6bf0245
flusteredbird <flusteredbird@gmail.com>
2015-11-10 14:46:00 -0800

    added upvote javascript animation and fixes

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,13 +1,10 @@
-Template.postsList.onRendered(function () {
-  this.find('.wrapper')._uihooks = {
-    insertElement: function (node, next) {
+Template.layout.onRendered(function() {
+  this.find('#main')._uihooks = {
+    insertElement: function(node, next) {
       $(node)
         .hide()
         .insertBefore(next)
         .fadeIn();
-    },
-    moveElement: function (node, next) {
-      //...
     },
     removeElement: function(node) {
       $(node).fadeOut(function() {
